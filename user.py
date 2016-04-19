2fd9151f3add1147635a090955c59f61cf83a739
georgesk <georgesk@debian.org>
2016-04-19 21:57:21 +0200

    first import

---
+++ user.py
@@ -0,0 +1,16 @@
+"""
+Provide a class User to deal with virtual class users.
+"""
+
+
+class User:
+
+    def __init__ (self):
+        self.givenName = None
+        self.surName   = None
+        self.email     = None
+        self.login     = None
+        self.password  = None
+        self.external  = None
+        return
+    
